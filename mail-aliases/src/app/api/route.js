c6397ac2d649935d0db665409bb549a1d3e24495
Michael Soh <2767724+sohmc@users.noreply.github.com>
2023-11-25 02:03:32 +0000

    alias operations now work!
    activate, deactivate, ignore all working

--- mail-aliases/src/app/api/route.js
+++ mail-aliases/src/app/api/route.js
@@ -7,21 +7,48 @@ export async function POST(request) {
   let returnObject = {};
 
   const formData = await request.formData();
-  const queryString = formData.get('query').toLowerCase();
 
-  console.log('route:request -- ' + JSON.stringify(formData.get('query')));
+  if (formData.get('query')) {
+    const queryString = formData.get('query').toLowerCase();
+    console.log('route:request -- ' + JSON.stringify(formData.get('query')));
 
-  if (queryString.startsWith('new:')) {
-    returnObject = await createAlias(queryString.slice(3));
-  } else {
-    returnObject = await queryDatabase(queryString);
+    if (queryString.startsWith('new:')) {
+      // remove 'new:'
+      returnObject = await createAlias(queryString.slice(3));
+    } else {
+      returnObject = await queryDatabase(queryString);
+    }
+
+    return Response.json(returnObject);
+  } else if (formData.get('action')) {
+    const action = formData.get('action');
+    const aliasUuid = formData.get('uuid');
+
+    if (['activate', 'deactivate', 'ignore'].indexOf(action) == -1)
+      return Response.json({'error': 'Invalid alias operation: ' + action});
+    if (!formData.get('uuid'))
+      return Response.json({'error': 'Invalid alias uuid: ' + aliasUuid});
+
+    returnObject = await aliasOperation(action, aliasUuid);
+    return Response.json(returnObject);
   }
-
-  return Response.json(returnObject);
 }
 
 async function createAlias(newAlias) {
+  const endpoint = '/alias';
+  const requestBody = {
+    'alias': newAlias,
+    'domain': subdomain,
+    'destination': ''
+  }
   return newAlias;
+}
+
+async function aliasOperation(action, aliasUuid) {
+  const endpoint = '/alias/' + aliasUuid + '/' + action
+
+  const responseJson = await sendApiRequest('GET', endpoint);
+  return responseJson;
 }
 
 
@@ -48,28 +75,34 @@ async function sendApiRequest(requestMethod = 'GET', endpoint, _payload = {}) {
     sha256: Sha256,
   });
 
-  // Check query endpoint for URL Parameters
-  const canonicalQueryObject = {};
-  const params = apiUrl.searchParams.toString().split('&');
-  params.forEach(element => {
-    const [p, v] = element.split('=');
-    canonicalQueryObject[decodeURIComponent(p)] = decodeURIComponent(v);
-  });
-
-  // Send Signing Request
-  const signed = await sigv4.sign({
+  // Build singing Payload
+  const signPayload = {
     method: requestMethod,
     hostname: apiUrl.host,
     path: apiUrl.pathname,
     protocol: apiUrl.protocol,
-    query: canonicalQueryObject,
-    // query: {"alias": "testing.trumpet@capricadev.tk"},
     headers: {
       'Content-Type': 'application/json',
       host: apiUrl.hostname,
     },
-  });
+  };
 
+  // Check query endpoint for URL Parameters
+  console.log('route:searchParams -- ' + apiUrl.searchParams.toString());
+  if (apiUrl.searchParams.toString().length > 0) {
+    const params = apiUrl.searchParams.toString().split('&');
+    const canonicalQueryObject = {};
+    
+    params.forEach(element => {
+      const [p, v] = element.split('=');
+      canonicalQueryObject[decodeURIComponent(p)] = decodeURIComponent(v);
+    });
+
+    signPayload.query = canonicalQueryObject;
+  }
+
+  // Get Signature
+  const signed = await sigv4.sign(signPayload);
   console.log('route:signedRequest -- ' + JSON.stringify(signed));
 
   const fetchResults = await fetch(apiUrl.href, {
--- mail-aliases/src/app/page.js
+++ mail-aliases/src/app/page.js
@@ -38,6 +38,30 @@ export default function OpenPage() {
     }
   }
 
+  async function aliasOperation(action, aliasUuid){
+    const formData = new FormData();
+    formData.append('action', action);
+    formData.append('uuid', aliasUuid);
+
+    const apiRequest = {
+      method: 'POST',
+      body: formData
+    };
+
+    const fetchResults = await fetch('/api', apiRequest);
+    const fetchResultsJson = await fetchResults.json();
+
+    if (fetchResultsJson.length === 1) {
+      const newResults = results.map(alias => {
+        if (alias.uuid == fetchResultsJson[0].uuid) 
+          return fetchResultsJson[0];
+        else return alias
+      });
+
+      setResults(newResults);
+    }
+  }
+
   const handleClose = () => setShowEmailModel(false);
   const handleShow = (emailAddress) => {
     setThisAlias(emailAddress);
@@ -48,10 +72,13 @@ export default function OpenPage() {
   if (results) {
     aliasList = results.map(alias =>
       <Accordion.Item eventKey={alias.uuid} key={alias.uuid}>
-        <Accordion.Header>{alias.fullEmailAddress} <GetIcons ignoreFlag={alias.ignore} activeFlag={alias.active} /></Accordion.Header>
+        <Accordion.Header>{alias.fullEmailAddress}<GetIcons ignoreFlag={alias.ignore} activeFlag={alias.active} /></Accordion.Header>
         <Accordion.Body>
           <p>Destination: {alias.destination}</p>
-          <Button onClick={ () => handleShow(alias.fullEmailAddress) }>Show Email</Button>
+          { !alias.active ||  alias.ignore ? <Button variant='success' onClick={ () => aliasOperation('activate', alias.uuid) }>Activate</Button> : null }{' '}
+          {  alias.active || !alias.ignore ? <Button variant='warning' onClick={ () => aliasOperation('deactivate', alias.uuid) }>Deactivate</Button> : null }{' '}
+          {  alias.active && !alias.ignore ? <Button variant='secondary' onClick={ () => aliasOperation('ignore', alias.uuid) }>Ignore</Button> : null }{' '}
+          <Button variant='info' onClick={ () => handleShow(alias.fullEmailAddress) }>Embiggen</Button>
         </Accordion.Body>
       </Accordion.Item>,
     );
@@ -79,14 +106,14 @@ function GetIcons({ ignoreFlag, activeFlag }) {
 
   if (!activeFlag) {
     icon = (
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope-x" viewBox="0 0 16 16">
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope-x" viewBox="0 0 16 16" title="Inactive alias">
         <path d="M2 2a2 2 0 0 0-2 2v8.01A2 2 0 0 0 2 14h5.5a.5.5 0 0 0 0-1H2a1 1 0 0 1-.966-.741l5.64-3.471L8 9.583l7-4.2V8.5a.5.5 0 0 0 1 0V4a2 2 0 0 0-2-2zm3.708 6.208L1 11.105V5.383zM1 4.217V4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v.217l-7 4.2z"/>
         <path d="M16 12.5a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0m-4.854-1.354a.5.5 0 0 0 0 .708l.647.646-.647.646a.5.5 0 0 0 .708.708l.646-.647.646.647a.5.5 0 0 0 .708-.708l-.647-.646.647-.646a.5.5 0 0 0-.708-.708l-.646.647-.646-.647a.5.5 0 0 0-.708 0"/>
       </svg>
     );
   } else if (ignoreFlag) {
     icon = (
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-incognito" viewBox="0 0 16 16">
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-incognito" viewBox="0 0 16 16" title="Alias is being ignored">
         <path fill-rule="evenodd" d="m4.736 1.968-.892 3.269-.014.058C2.113 5.568 1 6.006 1 6.5 1 7.328 4.134 8 8 8s7-.672 7-1.5c0-.494-1.113-.932-2.83-1.205a1.032 1.032 0 0 0-.014-.058l-.892-3.27c-.146-.533-.698-.849-1.239-.734C9.411 1.363 8.62 1.5 8 1.5c-.62 0-1.411-.136-2.025-.267-.541-.115-1.093.2-1.239.735m.015 3.867a.25.25 0 0 1 .274-.224c.9.092 1.91.143 2.975.143a29.58 29.58 0 0 0 2.975-.143.25.25 0 0 1 .05.498c-.918.093-1.944.145-3.025.145s-2.107-.052-3.025-.145a.25.25 0 0 1-.224-.274M3.5 10h2a.5.5 0 0 1 .5.5v1a1.5 1.5 0 0 1-3 0v-1a.5.5 0 0 1 .5-.5m-1.5.5c0-.175.03-.344.085-.5H2a.5.5 0 0 1 0-1h3.5a1.5 1.5 0 0 1 1.488 1.312 3.5 3.5 0 0 1 2.024 0A1.5 1.5 0 0 1 10.5 9H14a.5.5 0 0 1 0 1h-.085c.055.156.085.325.085.5v1a2.5 2.5 0 0 1-5 0v-.14l-.21-.07a2.5 2.5 0 0 0-1.58 0l-.21.07v.14a2.5 2.5 0 0 1-5 0v-1Zm8.5-.5h2a.5.5 0 0 1 .5.5v1a1.5 1.5 0 0 1-3 0v-1a.5.5 0 0 1 .5-.5"/>
       </svg>
     );
