3ec28c6a5230c3308a801a28fc3cd2d6ed3d5143
Michael Soh <2767724+sohmc@users.noreply.github.com>
2023-12-23 20:27:44 +0000

    updating middleware logic

--- mail-aliases/src/middleware.js
+++ mail-aliases/src/middleware.js
@@ -2,17 +2,22 @@ import { _NextRequest, NextResponse } from 'next/server';
 
 export function middleware(request) {
   const requestedUrl = request.nextUrl.clone();
+  const basePath = '/tmail';
 
   // const requestedHost = request.headers.get('X-Forwarded-Host');
-  const forwardedPath = request.headers.get('X-Forwarded-Path');
+  // const forwardedPath = request.headers.get('X-Forwarded-Path');
 
-  console.log('requestedUrl: ' + requestedUrl);
-  console.log('X-Forwarded-Path: ' + forwardedPath);
+  console.log('basePath set: ' + basePath);
+  console.log('requestedUrl: ' + requestedUrl.toString());
+  console.log('basePath index: ' + requestedUrl.toString().indexOf(basePath));
+  // console.log('X-Forwarded-Path: ' + forwardedPath);
 
-  if ((forwardedPath !== null) && (requestedUrl.toString().indexOf(forwardedPath) >= 0)) {
-    const pathForward = requestedUrl.toString().split(forwardedPath).pop();
+  if (requestedUrl.toString().indexOf(basePath) == -1) {
+    console.log('basePath not detected.');
+
+    const pathForward = basePath + requestedUrl.toString().split('localhost:3000').pop();
     console.log('Will forward to: ' + pathForward);
-    return NextResponse.redirect(new URL('/', pathForward));
+    return NextResponse.rewrite(new URL(pathForward));
   }
 
   // if (isProduction && requestedHost && !requestedHost.match(/example.com/)) {
